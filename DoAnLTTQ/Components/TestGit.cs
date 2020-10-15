164945dc07fa355e9a10f3f20dc52705a4d6501f
ADMIN <ADMIN@DESKTOP-U2BCLK6>
2020-10-15 19:42:43 +0700

    test git

---
+++ DoAnLTTQ/Components/TestGit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnLTTQ.Components
+{
+    public partial class TestGit : Component
+    {
+        public TestGit()
+        {
+            InitializeComponent();
+        }
+
+        public TestGit(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
