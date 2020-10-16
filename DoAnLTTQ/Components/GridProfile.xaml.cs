bdd6723070367d43daac45b66ea48d2942c73081
ADMIN <ADMIN@DESKTOP-U2BCLK6>
2020-10-16 15:14:20 +0700

    Hoan thanh GriddProfile Component

---
+++ DoAnLTTQ/Components/GridProfile.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoAnLTTQ.Components
+{
+    /// <summary>
+    /// Interaction logic for GridProfile.xaml
+    /// </summary>
+    public partial class GridProfile : UserControl
+    {
+        public GridProfile()
+        {
+            InitializeComponent();
+
+            List<imageSlot> data = new List<imageSlot>();
+            //data.Add(new imageSlot() { imgUri = "/Resources/Images/IMG_9715.png" });
+            for (int i = 0; i < 9; i++)
+            {
+                data.Add(new imageSlot() { imgUri = "/Resources/Images/IMG_9715.png" });
+            }
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            //data.Add(new imageSlot() { imgUri = "" });
+            listImage.DataContext = data;
+        }
+    }
+}
