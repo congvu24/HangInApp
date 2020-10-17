2df64dbe69683ba3643026bbf309fb19883e1350
vuggy17 <vuggy17@gmail.com>
2020-10-17 22:41:44 +0700

    add navigation icon

---
+++ DoAnLTTQ/Components/info_main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoAnLTTQ.Components
+{
+    /// <summary>
+    /// Interaction logic for info_main.xaml
+    /// </summary>
+    public partial class info_main : UserControl
+    {
+        public info_main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
