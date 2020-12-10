30f9943ca7f72560f19ff105ce39ea47eaf25665
nguyenkiet0807@gmail.com <nguyenkiet0807@gmail.com>
2020-12-10 21:31:06 +0700

    Check valid khi tao profile

--- DoAnLTTQ/Views/CreateProfileView.xaml.cs
+++ DoAnLTTQ/Views/CreateProfileView.xaml.cs
@@ -40,18 +40,21 @@ namespace DoAnLTTQ
         }
         private void Update_Click(object sender, RoutedEventArgs e)
         {
+            isProfileInvalid = isValidInputInformation();
             if (isButtonClicked && isProfileInvalid)
             {
                 CreateNewProfile();
+                UpdateNotification.Text = "Create profile successful";
                 button.Command = MaterialDesignThemes.Wpf.DialogHost.OpenDialogCommand;
             }
             else
             {
+                UpdateNotification.Text = "Create profile failed, check your profile again";
                 (sender as Button).Content = "Finish";
                 isButtonClicked = true;
             }
         }
-      
+
         private void CreateNewProfile()
         {
 
@@ -92,5 +95,142 @@ namespace DoAnLTTQ
             this.avatarLink = openFileDialog.FileName;
         }
         // kt xem profile có đúng chưa, nếu đúng thì set lại biến isProfileInvalid = true và set content của UpdateNotification thành Create Success
+
+        private bool isValidInputInformation()
+        {
+            if (!isValidNameInput(nameInput.Text))
+            {
+                errorNameInput.Visibility = Visibility.Visible;
+                errorNameInput.Text = "Vui lòng nhập tên hợp lệ! ";
+            }
+            else
+            {
+                errorNameInput.Visibility = Visibility.Hidden;
+            }
+
+            if (!isValidAgeInput(ageInput.Text))
+            {
+                errorAgeInput.Visibility = Visibility.Visible;
+                errorAgeInput.Text = "Vui lòng nhập tuổi hợp lệ! ";
+            }
+            else
+            {
+                errorAgeInput.Visibility = Visibility.Hidden;
+            }
+
+            if (!isValidSexInput(sexSelect.Text))
+            {
+                errorGenderInput.Visibility = Visibility.Visible;
+                errorGenderInput.Text = "Vui lòng chọn giới tính!";
+            }
+            else
+            {
+                errorGenderInput.Visibility = Visibility.Hidden;
+            }
+
+            if (!isValidHobbyInput(hobbyInput.Text))
+            {
+                errorHobbyInput.Visibility = Visibility.Visible;
+                errorHobbyInput.Text = "Vui lòng nhập sở thích hợp lệ";
+            }
+            else
+            {
+                errorHobbyInput.Visibility = Visibility.Hidden;
+            }
+
+            if (errorNameInput.Visibility != Visibility.Visible &&
+                errorAgeInput.Visibility != Visibility.Visible &&
+                errorHobbyInput.Visibility != Visibility.Visible &&
+                errorGenderInput.Visibility != Visibility.Visible)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isValidNameInput(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            else
+            {
+                byte[] asciiBytes = Encoding.ASCII.GetBytes(input);
+
+                for (int i = 0; i < asciiBytes.Length; i++)
+                {
+                    if (isSpecialKey(asciiBytes[i]))
+                        return false;
+                }
+
+                return true;
+            }
+
+        }
+        private bool isSpecialKey(byte key)
+        {
+            if (key >= 33 && key <= 47)
+                return true;
+            if (key >= 58 && key <= 64)
+                return true;
+            if (key == 95 || key == 96)
+                return true;
+            if (key >= 123 && key <= 126)
+                return true;
+
+            return false;
+        }
+
+        private bool isValidAgeInput(string ageString)
+        {
+            byte age = 1;
+            bool checkage = byte.TryParse(ageString, out age);
+
+            if (checkage)
+            {
+                if (age < 14 || age > 110)
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private bool isValidSexInput(string sexString)
+        {
+            if (sexString == "---Select--" || String.IsNullOrWhiteSpace(sexString))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool isValidHobbyInput(string input)
+        {
+            if (input.Length < 1)
+            {
+                return false;
+            }
+            else
+            {
+                byte[] asciiBytes = Encoding.ASCII.GetBytes(input);
+
+                for (int i = 0; i < asciiBytes.Length; i++)
+                {
+                    if (isSpecialKey(asciiBytes[i]))
+                        return false;
+                }
+
+                return true;
+            }
+        }
     }
 }
