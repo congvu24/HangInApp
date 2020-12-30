eb820b78c53232e77c39a613d6f6f2c6d52f94af
vuggy17 <vuggy17@gmail.com>
2020-12-30 08:35:56 +0700

    new file

---
+++ DoAnLTTQ/Views/LSetingViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DoAnLTTQ.Backend;
+
+namespace DoAnLTTQ.Components
+{
+    class LSetingViewModel : INotifyPropertyChanged
+    {
+        public string ProfileAvatarUrl { get; set; }
+        public Profile myProfile { get; set; }
+
+        public static LSetingViewModel Instance => new LSetingViewModel();
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        public LSetingViewModel()
+        {
+            User user = new User();
+            myProfile = user.myProfile;
+            ProfileAvatarUrl = user.myProfile.avatar.url;
+            MessageBox.Show(ProfileAvatarUrl);
+
+
+        }
+    }
+}
